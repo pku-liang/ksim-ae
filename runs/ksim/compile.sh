e4b6c8cd0779a230eb4ffbcc1330ceb2cc726f33
KEKE046 <KEKE_046@pku.edu.cn>
2023-06-29 18:18:43 +0800

    add ksim

---
+++ runs/ksim/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+design=$1
+. ../common.sh
+prepare_target_dir $design
+
+firtool --ir-hw --disable-all-randomization $design.fir -o $design.mlir
+timeit ksim $design.mlir -v -o $design.ll --out-header=$design.h --out-driver=$design.cpp 2>&1 | tee compile.log
+llc --stats --time-passes --relocation-model=dynamic-no-pic -O2 -filetype=obj $design.ll -o $design.o 2>&1 | tee compile.log
+clang++ -O2 $design.o $design.cpp -o $design
+
+provide_exe_file $design $design
