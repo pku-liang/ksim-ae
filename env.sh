f851549aaf19cd0e076ac5e960d2a33faf21805a
KEKE046 <KEKE_046@pku.edu.cn>
2023-06-30 12:11:57 +0800

    add verilator version fix

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 export TEST_ROOT=$PWD
-export PATH=$TEST_ROOT/sims/bin:$TEST_ROOT/tools/bin:$PATH
+export PATH=$TEST_ROOT/sims/bin:$TEST_ROOT/sims/install/bin:$TEST_ROOT/tools/bin:$PATH
 export SIMS="verilator ksim repcut-2 circt-verilator essent"
