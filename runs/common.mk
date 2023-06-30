349b23871ea1372854df70cffbf6a474241d308e
KEKE046 <KEKE_046@pku.edu.cn>
2023-06-30 08:40:34 +0800

    add commad to record fail runs

--- runs/common.mk
+++ runs/common.mk
@@ -1,10 +1,13 @@
 CASE_ROOT = $(TEST_ROOT)/cases
 
-$(shell mkdir bin obj)
+.phony: build clean show-fail
+
+$(shell [ -d bin ] || mkdir bin)
+$(shell [ -d obj ] || mkdir obj)
 build: $(patsubst $(CASE_ROOT)/%.fir,bin/%.out,$(wildcard $(CASE_ROOT)/*.fir))
 
 bin/%.out: $(CASE_ROOT)/%.fir $(EXTRA_DEPEND)
-	./compile.sh $*
+	./compile.sh $* || touch bin/$*.fail 
 
 clean:
 	rm -rf bin obj
