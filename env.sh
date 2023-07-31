f62f0e76d5c5262d44f8c3a5aef6e0a6a85a05f0
KEKE046 <KEKE_046@pku.edu.cn>
2023-07-31 19:51:16 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@ export PATH=$TEST_ROOT/sims/bin:$TEST_ROOT/sims/install/bin:$TEST_ROOT/tools/bin
 export SIMS="circt-verilator verilator-1 verilator-2 verilator-4 ksim essent repcut-1 repcut-2 repcut-4 repcut-6 repcut-8 vcs"
 
 _show_status() {
-  if ! which $1 > /dev/null; then
+  if ! which $1 2&>1 > /dev/null; then
     tput setaf 1
     printf "%s     \t" $1
     tput setaf 7
@@ -13,7 +13,7 @@ _show_status() {
     printf "%s     \t" $1
     tput setaf 7
     show_path=$(which $1)
-    if realpath --relative-to=$PWD $show_path 2>/dev/null >/dev/null; then
+    if realpath --relative-to=$PWD $show_path 2&>1 >/dev/null; then
       show_path=$(realpath --relative-to=$PWD $show_path)
     fi
     printf "is at %s\n" "$show_path"
