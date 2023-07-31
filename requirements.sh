e650675c6973cd93354f7e5017d467b0a68c13e5
KEKE046 <KEKE_046@pku.edu.cn>
2023-07-31 18:50:27 +0800

    update setup scripts

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman -S jre11-openjdk-headless sbt base-devel clang llvm python3 python-pip python-numpy python-pandas python-matplotlib python-seaborn cmake ninja 
+pacman -S jre11-openjdk-headless sbt base-devel clang llvm python3 python-pip python-numpy python-pandas python-matplotlib python-seaborn cmake ninja boost
