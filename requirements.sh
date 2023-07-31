517eb9273e6dec46fc139af4da701712072a7f83
KEKE046 <KEKE_046@pku.edu.cn>
2023-07-31 20:15:19 +0800

    update requirement

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,6 @@ pacman -S \
   wget \
   jre11-openjdk-headless sbt \
   base-devel clang llvm \
-  python3 python-pip python-numpy python-pandas python-matplotlib python-seaborn python-pathlib python-tqdm \
+  python3 python-pip python-numpy python-pandas python-matplotlib python-seaborn python-tqdm \
   cmake ninja boost ccache \
   flex bison help2man
