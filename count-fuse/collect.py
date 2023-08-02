1b5cea2ad363e4e0d4205c076a2db2f8df119db2
KEKE046 <KEKE_046@pku.edu.cn>
2023-08-02 10:09:31 +0800

    update num fused collect

--- count-fuse/collect.py
+++ count-fuse/collect.py
@@ -1,22 +1,19 @@
 from pathlib import Path
-import os
 import re
 
 df = []
-
 for file in Path('./obj').glob('*.txt'):
-  data = re.findall()
+  data = file.read_text()
+  init_cost = int(re.search(r'It: 0 Cost: (\d+)', data).group(1))
+  final_cost = int(re.search(r'FinalCost: (\d+)', data).group(1))
+  result = {
+    'name': file.stem,
+    'init': init_cost,
+    'opt': final_cost
+  }
+  df.append(result)
 
-cases = Path(os.environ['TEST_ROOT']).joinpath('cases')
-cases = [f.stem for f in cases.glob('*.fir')]
-obj_dir = Path('obj')
-
-for case in cases:
-  data = obj_dir.joinpath(case, 'compile.log').read_text()
-  init_cost = 0
-  min_cost = 1e9
-  for it, cost in re.findall(r'It: (\d+) Cost: (\d+)', data):
-    it, cost = map(int, (it, cost))
-    if it == 0: init_cost = cost
-    if cost < min_cost: min_cost = cost
-  print(case, min_cost, init_cost)
+import pandas as pd
+import os
+root = Path(os.environ['TEST_ROOT'])
+pd.DataFrame(df).to_csv(root.joinpath('results', 'fuse.csv'), index=False)
